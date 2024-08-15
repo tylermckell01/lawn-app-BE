c0ab5da3236aa1a5971773adb760e9fa0ddee52d
Tyler McKell <tmckell@devpipeline.com>
2024-08-15 10:56:49 -0600

    adds BE notes section in db

--- models/workouts.py
+++ models/workouts.py
@@ -12,25 +12,27 @@ class Workouts(db.Model):
     workout_id = db.Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
     workout_name = db.Column(db.String(), nullable=False)
     description = db.Column(db.String())
+    notes = db.Column(db.String())
     length = db.Column(db.Float(), nullable=False)
     user_id = db.Column(UUID(as_uuid=True), db.ForeignKey("Users.user_id"), nullable=False)
 
     exercises = db.relationship("Exercises", secondary=workouts_exercises_association_table, back_populates='workouts')
     user = db.relationship("Users", back_populates='workouts')
 
-    def __init__(self, workout_name, description, length, user_id):
+    def __init__(self, workout_name, description, notes, length, user_id):
         self.workout_name = workout_name
         self.description = description
+        self.notes = notes
         self.length = length
         self.user_id = user_id
 
     def new_workout_obj():
-        return Workouts('', '', 0, '')
+        return Workouts('', '', '', 0, '')
 
 
 class WorkoutsSchema(ma.Schema):
     class Meta:
-        fields = ['workout_id', 'workout_name', 'description', 'length', 'gym', 'exercises', 'user']
+        fields = ['workout_id', 'workout_name', 'description', 'notes', 'length', 'gym', 'exercises', 'user']
     user = ma.fields.Nested("UsersSchema", exclude=['workouts'])
     exercises = ma.fields.Nested('ExercisesSchema', many=True, exclude=['workouts'])
 
